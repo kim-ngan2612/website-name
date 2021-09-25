efa4e5f1e8879fac3d7ce907836a18fbdab233c6
KIMNGAN <18521137@gm.uit.edu.vn>
2021-09-25 20:58:50 +0700

    first commit

---
+++ js/login.js
@@ -0,0 +1,37 @@
+let user = document.getElementById("user");
+let password = document.getElementById("pwd");
+let eyeIcon = document.getElementById("eye-icon");
+let eye = document.getElementById("hide-eye");
+let eyeSlash = document.getElementById("hide-eye-slash");
+let login = document.getElementById("login");
+let hello = document.getElementById("hello");
+
+eyeIcon.onclick = function(){
+    if(password.type == "password"){
+        password.type ="text";
+        eye.style.display = "block";
+        eyeSlash.style.display = "none";
+    }
+    else{
+        password.type = "password";
+        eye.style.display= "none";
+        eyeSlash.style.display = "block";
+    }
+}
+function loginClick(){
+    let name = document.querySelector("#user-name");
+    login.style.display = 'none';
+    hello.style.display = 'block';
+    if(user.value != '')
+    {
+        name.innerHTML = user.value;
+    }
+    else{
+        name.innerHTML = 'Tên Đăng Nhập Rỗng';
+    }
+}
+// hàm đăng xuất
+function logoutClick(){
+    login.style.display = 'block';
+    hello.style.display = 'none';
+}
